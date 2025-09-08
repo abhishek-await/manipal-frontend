1bc730857cbc316e86fb7b41c225a65716d80d51
Abhishek <polishcow@LAPTOP-PAUARM50.>
2025-09-08 20:06:52 +0000

    FIX: minor

--- app/support-group/[id]/create-post/page.tsx
+++ app/support-group/[id]/create-post/page.tsx
@@ -2,16 +2,20 @@
 import React from "react";
 import { groupApi } from "@/features/support_groups/api/group.api";
 import CreatePostClient from "@/features/support_groups/CreatePostClient";
+import { cookies } from "next/headers";
 
 export default async function Page(props: PageProps<'/support-group/[id]/create-post'>) {
   const {id} = await props.params;
   const groupId = id
   if (!id) return <div>Group id missing</div>;
 
+  const cookieStore = await cookies()
+  const access = cookieStore.get('accessToken')?.value ?? ""
+
   // SSR: fetch minimal group info (title + avatar) to render header
   let rawGroup: any = null;
   try {
-    rawGroup = await groupApi.getGroup(id).catch(() => null);
+    rawGroup = await groupApi.getGroup(id,access).catch(() => null);
   } catch (e) {
     rawGroup = null;
   }

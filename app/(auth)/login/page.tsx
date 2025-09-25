c5ed63ded2a250541e2fb3df7154e8ba9539333e
Abhishek <polishcow@LAPTOP-PAUARM50.>
2025-09-25 19:07:46 +0000

    FIX: Build Issue

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,10 +1,19 @@
 import LoginForm from '@/features/auth/components/LoginForm'
-import React from 'react'
-
-export default function Login() {
+import React, { Suspense } from 'react'
+function Login() {
   return (
     <div className="flex w-full min-h-[100svh] justify-center items-start">
       <LoginForm />
     </div>
   );
 }
+
+const page = () => {
+  return (
+    <Suspense>
+      <Login />
+    </Suspense>
+  )
+}
+
+export default page
